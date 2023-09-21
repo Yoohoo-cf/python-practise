db6bca79fd821f28f7fbcc27ab601a8b209df960
Remy <wangrui777111@gmail.com>
2023-09-21 17:22:27 +0800

    part4.3

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
-import math
+"""
+Write a program that asks the user to enter numbers until they
+enter an empty string to quit. Finally, the program prints out
+the smallest and largest number from the numbers it received.
+"""
 
-radius = float(input("Enter the radius of the circle: "))
-area = math.pi * (radius ** 2)
 
-print(f"The area of the circle is {area:.2f}")
+smallest = None
+largest = None
 
+while True:
+
+    user = input("Enter a number (or press Enter to quit): ")
+
+    if user == "":
+        break
+
+    try:
+        number = float(user)
+
+        if smallest is None or number < smallest:
+            smallest = number
+        if largest is None or number > largest:
+            largest = number
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
+
+print(f"The smallest number you received is {smallest} and the largest one is {largest}")
 
 
